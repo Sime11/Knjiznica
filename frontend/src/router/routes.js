45c7cf724ef94d6e3a2dd9a56b81fd95661ba4e4
Sime Juresko <jureskosime@gmail.com>
2024-11-25 10:59:33 +0100

    dodan frontend

---
+++ frontend/src/router/routes.js
@@ -0,0 +1,21 @@
+const routes = [
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      { path: '/popis-knjiga', component: () => import('pages/PopisKnjigaPage.vue') },
+      { path: '/pretrazivanje', component: () => import('pages/TraziKnjiguPage.vue') },
+      { path: '/o-nama', component: () => import('pages/ONamaPage.vue') },
+      { path: '/lokacija', component: () => import('pages/LokacijaPage.vue') },
+      { path: '/login', component: () => import('pages/LoginPage.vue') },
+      { path: '/registracija', component: () => import('pages/RegistracijaPage.vue') }
+    ]
+  },
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/ErrorNotFound.vue')
+  }
+]
+
+export default routes
